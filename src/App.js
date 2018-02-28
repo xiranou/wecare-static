88f527c5253bc445e7395a2f9b3da22dfab75242
xiranou <xiranou@gmail.com>
2018-02-27 23:42:05 -0500

    wip

--- src/App.js
+++ src/App.js
@@ -2,7 +2,19 @@ import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
+import * as firebase from 'firebase';
+
 class App extends Component {
+  componentDidMount() {
+    this.initFirebase()
+  }
+
+  async initFirebase() {
+    const response = await fetch('/__/firebase/init.json');
+    const config = await response.text();
+    console.log(config);
+  }
+
   render() {
     return (
       <div className="App">
