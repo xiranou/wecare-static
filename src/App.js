dce8c1fd43851c0f6c02277cd88f9c4270e2b5a5
xiranou <xiranou@gmail.com>
2018-02-28 00:28:38 -0500

    wip firebase

--- src/App.js
+++ src/App.js
@@ -1,23 +1,31 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 import * as firebase from 'firebase';
 
 class App extends Component {
-  componentDidMount() {
+  componentWillMount() {
     this.initFirebase()
   }
 
   async initFirebase() {
-    // const response = await fetch('/__/firebase/init.json');
-    // const config = await response.text();
-    // console.log(config);
-    console.log(process.env.MOOKIE);
-        
+    if (process.env.NODE_ENV === 'production') {
+      const response = await fetch('/__/firebase/init.json');
+      firebase.initializeApp(response.json());
+      return;
+    }
+
+    const config = {
+      apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+      databaseURL: process.env.REACT_APP_FIREBASE_DATABASE_URL
+    };
+
+    firebase.initializeApp(config);
   }
 
   render() {
+    const database = firebase.database();
+    const ref = database.ref('test').once('value').then(snapshot => console.log(snapshot));
     return (
       <div className="App">
         WeCare?
